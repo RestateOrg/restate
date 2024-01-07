261c98b9a8d659c5ee6446256000a7f144135d8a
budda-manikanta-saaketh <115549218+budda-manikanta-saaketh@users.noreply.github.com>
2024-01-07 14:48:23 +0530

    Upload_project page 50%

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:restate/Builder/Upload_project.dart';
 import 'package:restate/Builder/builderHome.dart';
 import 'package:restate/Machinery/machineryHome.dart';
 import 'package:restate/Materials/materialHome.dart';
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Begin(),
+      home: const UploadProject(),
       routes: {
         '/Getstarted/': (context) => const GetStarted(),
         '/login/': (context) => const LoginView(),
